ae45115189cb09d1b6bad942746c1262b6a07325
jimypage0211 <jimypage@bu.edu>
2024-10-05 10:54:53 -0400

    profile pic

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,8 +1,26 @@
 import React from "react";
 import classes from "../styles/layout.module.css";
+import Image from "next/image";
 
 function Layout({ children }) {
-    return <div className={classes.container}>{children}</div>;
+    return (
+        <div>
+            <div className={classes.profileWrapper}>
+                <div className={classes.profileImageWrapper}>
+                    <Image
+                        className={classes.profileImage}
+                        src="/profile-image.jpg"
+                        width={150}
+                        height={150}
+                    />
+                </div>
+                <div className={classes.profileTextWrapper}>
+                    <p className={classes.profileNameText}>Jesus Suarez</p>
+                </div>
+            </div>
+            <div className={classes.container}>{children}</div>
+        </div>
+    );
 }
 
 export default Layout;
